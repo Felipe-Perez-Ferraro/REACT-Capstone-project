c744c4af76930dca2d8819f4c546b32c2a44c350
Feliperto <fperezferraro@gmail.com>
2023-08-31 18:08:15 -0300

    created personalized stylings

---
+++ src/tailwindStyles.js
@@ -0,0 +1,2 @@
+export const buttonStyle = 'border border-slate-100 rounded-md p-5 bg-pink-600 even:bg-pink-700 text-white font-bold shadow-lg shadow-pink-500/40 h-28 text-xl hover:bg-pink-700 even:hover:bg-pink-800';
+export const screenStyle = 'max-w-xs sm:max-w-md md:max-w-2xl lg:max-w-4xl xl:max-w-6xl mx-auto';
