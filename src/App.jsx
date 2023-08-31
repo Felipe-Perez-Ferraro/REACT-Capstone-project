299924d6935f46f75e8f216a4dd341c6d1af382b
Feliperto <fperezferraro@gmail.com>
2023-08-31 12:28:18 -0300

    Updated jsx files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  const str = 'Hello';
+
+  return (
+    <div className="text-left">
+      <h1>
+        Hello
+        {str}
+      </h1>
+    </div>
+  );
+}
+
+export default App;
