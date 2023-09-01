e82ee2562f7d4d5ad89cfe86c3859a98e43d87e7
Felipe <fperezferraro@gmail.com>
2023-09-01 18:29:43 -0300

    Removed axios

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,10 @@
-import axios from 'axios';
-
 const apiKey = '04ecc2840ecc44887bf18fa4b6327d8e';
 
 const fetchApi = async (symbol) => {
   try {
-    const res = await axios.get(`https://financialmodelingprep.com/api/v3/income-statement/${symbol}?limit=120&apikey=${apiKey}`);
-    return res.data;
+    const res = await fetch(`https://financialmodelingprep.com/api/v3/income-statement/${symbol}?limit=120&apikey=${apiKey}`);
+    const data = res.json();
+    return data;
   } catch (error) {
     throw new Error(error);
   }
