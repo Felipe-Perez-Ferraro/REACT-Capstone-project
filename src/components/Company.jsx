b5147ef6517684d869ba0d773de5f6913662ba1c
Feliperto <fperezferraro@gmail.com>
2023-09-04 13:12:12 -0300

    Added the loading and error status

--- src/components/Company.jsx
+++ src/components/Company.jsx
@@ -9,6 +9,8 @@ function Company() {
   const { symbol } = useParams();
   const { companies } = useSelector((state) => state.companies);
   const { isFetched } = useSelector((state) => state.companies);
+  const { isLoading } = useSelector((state) => state.companies);
+  const { error } = useSelector((state) => state.companies);
   const symbolSelected = companiesArray.find((company) => company.symbol === symbol);
   const dispatch = useDispatch();
 
@@ -16,25 +18,41 @@ function Company() {
     if (!isFetched) {
       dispatch(fetchCompaniesThunk(symbolSelected.symbol));
     }
-  }, [fetchCompaniesThunk, isFetched]);
+  }, [dispatch, isFetched, symbolSelected.symbol]);
 
   return (
     <section className="section bg-pink-900 py-7">
       <article className={`${screenStyle} flex flex-col gap-2`}>
-        <h2 className="text-center text-slate-100 font-bold text-4xl">
-          {symbol === 'AAPL' && 'Apple'}
-          {symbol === 'GOOG' && 'Google'}
-        </h2>
-        <p className="text-center font-semibold text-2xl text-slate-100 mb-5">Check the financial insights of the company in the last 5 years.</p>
-        <div className="grid grid-cols-2 last:col-span-2 gap-3">
-          {companies.map((company) => (
-            <Link className="last:col-span-2" to={`/insights/${symbol}/${company.id}`} key={company.id}>
-              <button className={`${buttonStyle} w-full`} type="button">
-                {company.calendarYear}
-              </button>
-            </Link>
-          ))}
-        </div>
+        {isLoading && (
+          <div>
+            <p className="text-2xl text-center font-bold text-slate-100">Loading...</p>
+          </div>
+        )}
+        {!isLoading && (
+          <>
+            <h2 className="text-center text-slate-100 font-bold text-4xl">
+              {symbol === 'AAPL' && 'Apple'}
+              {symbol === 'GOOG' && 'Google'}
+            </h2>
+            <p className="text-center font-semibold text-2xl text-slate-100 mb-5">
+              Check the financial insights of the company in the last 5 years.
+            </p>
+            <div className="grid grid-cols-2 last:col-span-2 gap-3">
+              {companies.map((company) => (
+                <Link
+                  className="last:col-span-2"
+                  to={`/insights/${symbol}/${company.id}`}
+                  key={company.id}
+                >
+                  <button className={`${buttonStyle} w-full`} type="button">
+                    {company.calendarYear}
+                  </button>
+                </Link>
+              ))}
+            </div>
+          </>
+        )}
+        {error && <div>{error}</div>}
       </article>
     </section>
   );
